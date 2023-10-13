5c2a183eb2088687a27e41a0a1762116a8bb3e7e
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2023-10-13 17:56:10 +0700

    Свёрстан TeacherList

--- src/components/teacher/TeacherList/TeacherList.jsx
+++ src/components/teacher/TeacherList/TeacherList.jsx
@@ -3,16 +3,44 @@ import st from "./TeacherList.module.scss"
 import TeacherCard from '../TeacherCard/TeacherCard'
 import img from "../../../assets/images/teacher/teacher.png"
 
-const teacher = {
+const teachers = [{
+  id: 0,
   name: "Альберт Васильев",
   description: "Преподаватель немецкого языка",
   img: img,
-}
+},
+{
+  id: 1,
+  name: "Альберт Васильев",
+  description: "Преподаватель немецкого языка",
+  img: img,
+},
+{
+  id: 2,
+  name: "Альберт Васильев",
+  description: "Преподаватель немецкого языка",
+  img: img,
+},
+{
+  id: 3,
+  name: "Альберт Васильев",
+  description: "Преподаватель немецкого языка",
+  img: img,
+},
+{
+  id: 4,
+  name: "Альберт Васильев",
+  description: "Преподаватель немецкого языка",
+  img: img,
+},
+]
 
 const TeacherList = () => {
   return (
     <div className={st.list}>
-        <TeacherCard teacher={teacher}/>
+      {
+        teachers.map(teacher => <TeacherCard teacher={teacher} key={teacher.id}/>)
+      }
     </div>
   )
 }
