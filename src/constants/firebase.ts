ca16ea12234bcfd1e15569279793a4034215b844
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-03-21 10:34:58 +0700

    fix firebase-messaging-sw

--- src/constants/firebase.ts
+++ src/constants/firebase.ts
@@ -10,18 +10,6 @@ export const firebaseConfig = {
     appId: "1:464394570384:android:9be3c036db2af5ec82a615"
 };
 
-// export const firebaseConfig = {
-//     apiKey: "AIzaSyCXlTPxSCBii6XRjOpgHJ5eHFyAB4YOum0",
-//   authDomain: "test-project-cd7d7.firebaseapp.com",
-//   projectId: "test-project-cd7d7",
-//   storageBucket: "test-project-cd7d7.appspot.com",
-//   messagingSenderId: "738235412840",
-//   appId: "1:738235412840:web:7fda7d8e44efedb00026dd",
-//   measurementId: "G-44J596HQWV"
-// };
-
-
-
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseMessaging = getMessaging(firebaseApp);
 
