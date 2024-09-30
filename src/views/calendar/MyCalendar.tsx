0c041c32bd45fcd55f7ac771649fb1b49c3e41fd
DeadDPie <elizabethfox434@gmail.com>
2024-09-30 18:52:35 +0700

    почистила код

--- src/views/calendar/MyCalendar.tsx
+++ src/views/calendar/MyCalendar.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import "moment/locale/ru"; // локализация для русского языка
-import "react-big-calendar/lib/css/react-big-calendar.css"; // импорт стилей по умолчанию
-import "./MyCalendar.scss"; // ваш файл со стилями
+import "moment/locale/ru"; 
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./MyCalendar.scss"; 
 import { messages } from "../../constants/calendarMessages";
-// Локализация для календаря
+
 const localizer = momentLocalizer(moment);
 
 const MyCalendar = () => {
-  // Пример событий
+
   const [events, setEvents] = useState([
     {
       title: "Событие 1",
@@ -31,9 +31,8 @@ const MyCalendar = () => {
         startAccessor="start"
         endAccessor="end"
         messages={messages}
-        // style={{ height: "80%", width: "90%" }}
-        onSelectSlot={(slotInfo: any) => alert(`Вы выбрали: ${slotInfo.start}`)} // Обработка клика по дате
-        selectable // Делает ячейки "выбираемыми"
+        onSelectSlot={(slotInfo: any) => alert(`Вы выбрали: ${slotInfo.start}`)} 
+        selectable 
       />
     </div>
   );
